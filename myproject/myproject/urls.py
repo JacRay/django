4a7df6b9307c378571c242e4664dfc887f138d9e
JacRay <jacobraybin@gmail.com>
2021-12-11 11:44:46 +0530

    urlpatterns and path

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    #to call the urls.py file in the myapp
+    path('',include('myapp.urls')),
 ]
