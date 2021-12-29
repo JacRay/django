9003db62c08c9491043fba369df25df1b9826db4
JacRay <jacobraybin@gmail.com>
2021-12-29 15:13:01 +0530

    Modules in django

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Feature
 # Create your views here.
 def index(request):
     
@@ -20,7 +21,45 @@ def index(request):
     # name = 'John'
     # return render(request, 'index.html',context) 
 
-    return render(request, 'index.html')  
+
+    #making a object of Feature class
+    # feature1 = Feature()
+    # #giving values
+    # feature1.id = 0
+    # feature1.name = 'John'
+    # feature1.details = ' Our service is very quick'
+    # #the object is send to the html with a new name feature
+    # return render(request, 'index.html', {'feature': feature1 })  
+
+    feature1 = Feature()
+    #giving values
+    feature1.id = 0
+    feature1.name = 'Fast'
+    feature1.details = ' Our service is very quick'
+
+    feature2 = Feature()
+    #giving values
+    feature2.name = 'Easy to Use'
+    feature2.id = 1
+    feature2.details = ' Our service is Easy to use'
+
+    feature3 = Feature()
+    #giving values
+    feature3.id = 2
+    feature3.name = 'Affordable'
+    feature3.details = ' Our service is very affordable'
+
+    feature4 = Feature()
+    #giving values
+    feature4.id = 3
+    feature4.name = 'Fast'
+    feature4.details = ' Our service is very very quick'
+    # when there are multiple same type of modes we can use django list
+    # make it easy using fro loop in index.html
+    features = {feature1, feature2, feature3, feature4}
+
+    return render(request, 'index.html', {'features': features})
+
 
 def counter(request):
     
