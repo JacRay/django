5d8526c16fa48500a4f10f683e982e092f4bcdce
JacRay <jacobraybin@gmail.com>
2021-12-29 15:43:28 +0530

    Admin

--- myproject/myapp/admin.py
+++ myproject/myapp/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from .models import Feature
 # Register your models here.
+#have to register modules here
+admin.site.register(Feature)
--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -1,7 +1,13 @@
 from django.db import models
 
 # Create your models here.
-class Feature:
-    id: int
-    name:str
-    details: str
+# class Feature:
+#     id: int
+#     name:str
+#     details: str
+
+
+class Feature(models.Model):
+    #will have default id so no id needed
+    name = models.CharField(max_length=100)
+    details= models.CharField(max_length=500)
